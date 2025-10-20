d5cc3dc8c15ff60df43a7da36b685410dc483e5c
siddhantthakur278-bit <siddhantthakur278@gmail.com>
2025-10-20 21:50:49 +0530

    added some styles

--- index.js
+++ index.js
@@ -127,3 +127,19 @@ var cursor = {
 }
 
 cursor.init();
+const themeButtons = document.querySelectorAll('.theme-btn');
+
+themeButtons.forEach(btn => {
+  btn.addEventListener('mousemove', e => {
+    const rect = btn.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    btn.style.setProperty('--x', x + 'px');
+    btn.style.setProperty('--y', y + 'px');
+  });
+  
+  btn.addEventListener('mouseleave', () => {
+    btn.style.setProperty('--x', '50%');
+    btn.style.setProperty('--y', '50%');
+  });
+});
